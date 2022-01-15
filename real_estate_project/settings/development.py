f5fb122ad8236d82e6b569741075d9c524c53c8c
BuryPink <rileygam2000@gmail.com>
2022-01-15 13:09:27 +1300

    Chore: Django Login

--- real_estate_project/settings/development.py
+++ real_estate_project/settings/development.py
@@ -3,13 +3,10 @@ from .base import *
 DATABASES = {
     'default': {
         'ENGINE': env("POSTGRES_ENGINE"),
-        'NAME': env("POSTGRES_NAME"),
+        'NAME': env("POSTGRES_DB"),
         'USER': env("POSTGRES_USER"),
         'PASSWORD': env("POSTGRES_PASSWORD"),
         'HOST': env("PG_HOST"),
         'PORT': env("PG_PORT"),
     }
 }
-
-# 'ENGINE': 'django.db.backends.sqlite3',
-# 'NAME': BASE_DIR / 'db.sqlite3',
